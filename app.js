7c52b54bf930f1414ec5950d535b6ef433bbcb99
abhinildutt <abhinil.dutt@gmail.com>
2022-10-15 22:20:40 -0500

    push 3

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
+app.use('/', (req, res, next) => {
+    console.log("This always runs");
+    next();
+});
 
-const server = http.createServer(routes);
+app.use('/add-product', (req, res, next) => {
+    console.log("In the middelware 2");
+    res.send("<h1> The \"Add product\" page </h1>")
+});
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+    console.log("In the middelware 3");
+    res.send("<h1> hello from express ! </h1>")
+});
+
+app.listen(3000);

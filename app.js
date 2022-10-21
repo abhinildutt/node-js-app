b75a8c337ed7bd85bbe7ada064b8d2c5418e3c7e
abhinildutt <abhinil.dutt@gmail.com>
2022-10-21 18:10:39 -0500

    routes folder

--- app.js
+++ app.js
@@ -4,19 +4,16 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action = "/product" method = "POST"><input type = "text" name = "title"> <button type = "submit"> ADD PRODUCT ! </button> </input> </form>')
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-}) 
-
-app.use('/', (req, res, next) => {
-    res.send("<h1> Hello from express ! </h1>")
-});
+app.use((req, res, next) => {
+    res.send('<h1> Page not found </h1>')
+})
 
 app.listen(3000);

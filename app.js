f2c9e799a5268d9317861948ff3c6833073623c0
abhinildutt <abhinil.dutt@gmail.com>
2022-10-21 17:40:21 -0500

    parse body requests

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log("This always runs");
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-    console.log("In the middelware 2");
-    res.send("<h1> The \"Add product\" page </h1>")
+    res.send('<form action = "/product" method = "POST"><input type = "text" name = "title"> <button type = "submit"> ADD PRODUCT ! </button> </input> </form>')
 });
 
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+}) 
+
 app.use('/', (req, res, next) => {
-    console.log("In the middelware 3");
-    res.send("<h1> hello from express ! </h1>")
+    res.send("<h1> Hello from express ! </h1>")
 });
 
 app.listen(3000);

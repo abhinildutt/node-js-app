431558e5427bfe8a3fd2ff4b35122f0ef86aa6bc
abhinildutt <abhinil.dutt@gmail.com>
2022-10-23 17:39:17 -0500

    path

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const http = require('http');
+
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser')
 
@@ -9,11 +11,11 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.send('<h1> Page not found </h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000);
